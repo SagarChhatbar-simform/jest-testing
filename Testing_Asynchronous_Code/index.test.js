6693ac1c93fc4ff08cef0b6fd0d65bf2900af63d
SagarChhatbar-simform <sagar.chhatbar@simformsolutions.com>
2022-09-12 10:37:28 +0530

    added some more tests

--- Testing_Asynchronous_Code/index.test.js
+++ Testing_Asynchronous_Code/index.test.js
@@ -19,9 +19,27 @@ test('the data is peanut butter', () => {
    fetchData(callback)
   });   
 
-  test("should test async", ()=>{
-      setTimeout(()=>{
-        expect(1).toBe(2);
-      },2000)
+  const add = (a,b)=>{
+      return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(a<0||b<0)
+            reject(new Error('number should be no-negative'))
+            resolve(a+b);
+          },2000)
+      })
+  }
+
+
+  test("should return sum",(done)=>{
+      add(3,2)
+      .then(sum => expect(sum).toBe(5))
+      done()
   })
+
+//   test("should test async", (done)=>{
+//       setTimeout(()=>{
+//         expect(1).toBe(2);
+//         done()
+//       },2000)
+//   })
    
